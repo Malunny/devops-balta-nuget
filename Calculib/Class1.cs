dcb162a033630fe113dd0562b1e3accf62664a99
Malunny <jhonnybravocf@gmail.com>
2026-06-13 12:12:47 -0300

    feat: Sum and Sum TestMethod

---
+++ Calculib/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Calculib;
+
+public class CalcLib
+{
+    public decimal Sum(params IEnumerable<decimal> numbers)
+    {
+        List<decimal> numbersList = numbers.ToList();
+        
+        decimal sum = numbersList.Sum();
+
+        return sum;
+    }
+}
---
+++ CalculibTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Calculib;
+
+namespace Calculib.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void ShouldCalculateSum()
+    {
+        decimal[] a = [1, 2, 3, 4, 5, 6];
+        decimal[] b = [2, 5, 8];
+
+        var calc = new CalcLib();
+
+        var responseA = calc.Sum(a);
+        var responseB = calc.Sum(b);
+        var responseAB = calc.Sum(a.Concat(b));
+        
+        bool aResult = responseA == 21;
+        bool bResult = responseB == 15;
+        bool abResult = responseAB == 36;
+        
+        bool passed =  aResult && bResult && abResult;
+        
+        Assert.That(passed, Is.True);
+    }
+}
